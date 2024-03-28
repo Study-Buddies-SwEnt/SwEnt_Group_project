023f24fff3a3179697b88f24ae8484404308f523
TdVBd <117034216+TdVBd@users.noreply.github.com>
2024-03-28 11:46:15 +0100

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "swent-group_study-budy")
+        property("sonar.organization", "swent-group")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
