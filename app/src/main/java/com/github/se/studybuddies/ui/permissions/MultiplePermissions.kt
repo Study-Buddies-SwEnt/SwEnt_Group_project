50c114c44a7c45e263e03d5d2f931cdc9cae5d4f
lorie <freez3moon@gmail.com>
2024-04-07 13:52:35 +0200

    Created Permissions class to handle permissions and PermissionCode to easily identify which permissions are requested

---
+++ app/src/main/java/com/github/se/studybuddies/ui/permissions/MultiplePermissions.kt
@@ -0,0 +1,61 @@
+package com.github.se.studybuddies.ui.permissions
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.github.se.studybuddies.ui.permissions.PermissionsCode.CAMERA_PERMISSION_CODE
+import com.github.se.studybuddies.ui.permissions.PermissionsCode.STORAGE_PERMISSION_CODE
+
+class MultiplePermissions {
+
+    companion object {
+        private const val CAMERA = Manifest.permission.CAMERA
+        private const val MIC = Manifest.permission.RECORD_AUDIO
+    }
+
+
+
+    // Function to check and request permission.
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        } else {
+            Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    // This function is called when the user accepts or decline the permission.
+// Request Code is used to check which permission called this function.
+// This request code is provided when the user is prompt for permission.
+    fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Camera Permission Granted", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this@MainActivity, "Camera Permission Denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        } else if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Storage Permission Granted", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this@MainActivity, "Storage Permission Denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+}
