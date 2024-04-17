1d5bb8cbddee2999ba2e4708fa1a06c76a5753f1
lorie <freez3moon@gmail.com>
2024-04-17 22:18:08 +0200

    Cleaned up code

--- app/src/main/java/com/github/se/studybuddies/ui/groups/CreateGroup.kt
+++ app/src/main/java/com/github/se/studybuddies/ui/groups/CreateGroup.kt
@@ -84,7 +84,6 @@ fun CreateGroup(groupViewModel: GroupViewModel, navigationActions: NavigationAct
                 verticalArrangement = Arrangement.spacedBy(20.dp),
                 horizontalAlignment = Alignment.CenterHorizontally) {
                   CenterAlignedTopAppBar(
-                      modifier = Modifier.testTag("SettingInput"),
                       title = { Sub_title(stringResource(R.string.create_group)) },
                       navigationIcon = {
                         Icon(
