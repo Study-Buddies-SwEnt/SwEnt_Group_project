3965b74498f122a92aaf19ba6ae4f3e1f5c13c24
OliverPC <oollii@live.fr>
2024-04-18 23:34:41 +0200

    Reformated

--- app/src/androidTest/java/com/github/se/studybuddies/tests/GroupInvitationTest.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/tests/GroupInvitationTest.kt
@@ -1,29 +1,25 @@
 package com.github.se.studybuddies.tests
 
-import com.github.se.studybuddies.utility.FirebaseUtils
 import com.github.se.studybuddies.utility.FirebaseUtils.createGroupInviteLink
-import com.google.android.apps.common.testing.accessibility.framework.replacements.Uri
-import com.google.firebase.dynamiclinks.DynamicLink
-import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class GroupInvitationTest {
 
-    @Test
-    fun createLink() {
-        val result = createGroupInviteLink("validGroupUID")
-        assertEquals(("https://example.page.link/group?groupUID=validGroupUID"), result)
-    }
+  @Test
+  fun createLink() {
+    val result = createGroupInviteLink("validGroupUID")
+    assertEquals(("https://example.page.link/group?groupUID=validGroupUID"), result)
+  }
 
-    //can't be null ?
-    /*@Test(expected = IllegalArgumentException::class)
-    fun NullGroupUID() {
-        createGroupInviteLink(null)
-    }*/
+  // can't be null ?
+  /*@Test(expected = IllegalArgumentException::class)
+  fun NullGroupUID() {
+      createGroupInviteLink(null)
+  }*/
 
-    @Test(expected = IllegalArgumentException::class)
-    fun EmptyGroupUID() {
-        createGroupInviteLink("")
-    }
+  @Test(expected = IllegalArgumentException::class)
+  fun EmptyGroupUID() {
+    createGroupInviteLink("")
+  }
 }
