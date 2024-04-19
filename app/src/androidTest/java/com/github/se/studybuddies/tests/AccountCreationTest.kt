8e52ccdb7534521a6f7fd251fbd53903e021a50b
Svannia <75037332+Svanniah@users.noreply.github.com>
2024-04-19 17:16:51 +0200

    corrected a test

--- app/src/androidTest/java/com/github/se/studybuddies/tests/AccountCreationTest.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/tests/AccountCreationTest.kt
@@ -3,17 +3,14 @@ package com.github.se.studybuddies.tests
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.studybuddies.navigation.NavigationActions
-import com.github.se.studybuddies.navigation.Route
 import com.github.se.studybuddies.ui.settings.CreateAccount
 import com.github.se.studybuddies.viewModels.UserViewModel
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.mockk.confirmVerified
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
-import io.mockk.verify
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
