adef6b26d139aae07fcc3b7db0a91e46a75ddf44
lorie <freez3moon@gmail.com>
2024-04-19 10:04:17 +0200

    reformatting

--- app/src/androidTest/java/com/github/se/studybuddies/screens/CreateGroupScreen.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/screens/CreateGroupScreen.kt
@@ -2,7 +2,6 @@ package com.github.se.studybuddies.screens
 
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.github.kakaocup.compose.node.element.KNode
 
 class CreateGroupScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<CreateGroupScreen>(
