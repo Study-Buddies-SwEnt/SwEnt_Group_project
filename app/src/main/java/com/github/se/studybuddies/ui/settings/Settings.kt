449d33d23f70bd271e535e0d34f2fa166a0c5a2e
Océane Voland <oceane.voland@epfl.ch>
2024-04-19 15:31:58 +0200

    Correct the settings screen

--- app/src/main/java/com/github/se/studybuddies/ui/settings/Settings.kt
+++ app/src/main/java/com/github/se/studybuddies/ui/settings/Settings.kt
@@ -1,23 +1,36 @@
 package com.github.se.studybuddies.ui.settings
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.github.se.studybuddies.navigation.NavigationActions
 import com.github.se.studybuddies.ui.GoBackRouteButton
 import com.github.se.studybuddies.ui.Main_title
 import com.github.se.studybuddies.ui.TopNavigationBar
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Settings(backRoute: String, navigationActions: NavigationActions) {
-  Column {
-    TopNavigationBar(
-        title = { Main_title(title = "Settings") },
-        navigationIcon = {
-          GoBackRouteButton(navigationActions = navigationActions, backRoute = backRoute)
-        }) {}
-    Text("Settings")
-  }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar =
+        {
+            TopNavigationBar(title = { Main_title(title = "Settings") },
+                navigationIcon = {
+                    GoBackRouteButton(
+                        navigationActions = navigationActions,
+                        backRoute = backRoute
+                    )
+                },
+                actions = {})
+        }
+    ) {
+        Text("Settings")
+    }
 }
