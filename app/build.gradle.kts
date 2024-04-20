4c35eac71c6bacc2a4f8dd7ceb010d5d8321679d
lorie <freez3moon@gmail.com>
2024-04-20 18:32:22 +0200

    Added unit testing of checkPermission function and corresponding library in the build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ android {
                 useLegacyPackaging = true
             }
         }
+        unitTests.isReturnDefaultValues = true
     }
 }
 
@@ -132,6 +133,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
 
     // Dependencies for using MockK in instrumented tests
+    testImplementation("io.mockk:mockk:1.13.7")
     androidTestImplementation("io.mockk:mockk:1.13.7")
     androidTestImplementation("io.mockk:mockk-android:1.13.7")
     androidTestImplementation("io.mockk:mockk-agent:1.13.7")
---
+++ app/src/test/java/com/github/se/studybuddies/PermissionsUnitTest.kt
@@ -0,0 +1,50 @@
+package com.github.se.studybuddies
+
+import android.content.Context
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import com.github.se.studybuddies.ui.permissions.checkPermission
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(MockitoJUnitRunner::class)
+class PermissionsUnitTest {
+
+  @Mock
+  private lateinit var mockContext :Context
+  private lateinit var mockLauncher: ManagedActivityResultLauncher<String, Boolean>
+
+
+  @Test
+  fun permissionsRequestLaunched() {
+    val permission = "android.permission.READ_MEDIA_IMAGES"
+    mockLauncher = mockk<ManagedActivityResultLauncher<String, Boolean>>(relaxed = true){
+      launch(permission)
+    }
+    /*var requestGranted = false
+    val requestPermissionLauncher =
+      rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+          requestGranted = true
+        }
+      }
+     */
+
+    mockContext = mockk<Context>(relaxed = true){}
+
+    checkPermission(mockContext, permission, mockLauncher)
+    verify { mockLauncher.launch(permission) }
+
+  }
+}
