be8181016fdead9024ec2d621e3c55cb36467e06
lorie <freez3moon@gmail.com>
2024-04-20 19:40:01 +0200

    Debugged unit test when called twice

--- app/src/test/java/com/github/se/studybuddies/PermissionsUnitTest.kt
+++ app/src/test/java/com/github/se/studybuddies/PermissionsUnitTest.kt
@@ -1,13 +1,16 @@
 package com.github.se.studybuddies
 
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.core.content.ContextCompat
 import com.github.se.studybuddies.ui.permissions.checkPermission
+import io.mockk.clearMocks
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 /**
@@ -18,7 +21,7 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class PermissionsUnitTest {
 
-  @Mock private lateinit var mockContext: Context
+  @RelaxedMockK private lateinit var mockContext: Context
   private lateinit var mockLauncher: ManagedActivityResultLauncher<String, Boolean>
 
   @Test
@@ -30,17 +33,26 @@ class PermissionsUnitTest {
 
     checkPermission(mockContext, permission, mockLauncher)
     verify { mockLauncher.launch(permission) }
+    tearDown()
   }
 
   @Test
   fun permissionsRequestNotLaunched() {
-    val permission = "android.permission.READ_MEDIA_IMAGES"
+    val permission = "android.permission.CALENDAR"
     mockLauncher =
         mockk<ManagedActivityResultLauncher<String, Boolean>>(relaxed = true) { launch(permission) }
     mockContext = mockk<Context>(relaxed = true) {}
 
     checkPermission(mockContext, permission, mockLauncher)
+    assert(
+        ContextCompat.checkSelfPermission(mockContext, permission) ==
+            PackageManager.PERMISSION_GRANTED)
     checkPermission(mockContext, permission, mockLauncher)
     verify(exactly = 1) { mockLauncher.launch(permission) }
+    tearDown()
+  }
+
+  private fun tearDown() {
+    clearMocks(mockContext, mockLauncher)
   }
 }
