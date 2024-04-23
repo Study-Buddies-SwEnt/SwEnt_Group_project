ef33ed0847a867eceefcb017469bb0783e7456bb
lorie <freez3moon@gmail.com>
2024-04-23 20:11:02 +0200

    reverted build.gradle file that was making other tests fail with the 'ConnectedAndroidTest'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,24 @@
-import org.jetbrains.kotlin.codegen.intrinsics.ArrayOf
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.ncorti.ktfmt.gradle") version "0.16.0"
     id("com.google.gms.google-services")
-    id("org.sonarqube") version "4.4.1.3373"
 }
-
-sonar {
-    properties {
-        property("sonar.projectKey", "Study-Buddies-SwEnt_SwEnt_Group_project")
-        property("sonar.organization", "study-buddies-swent")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
-
 android {
     namespace = "com.github.se.studybuddies"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.github.se.studybuddies"
         minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
-        testInstrumentationRunnerArguments += mapOf(
-            "clearPackageData" to "true",
-        )
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -54,15 +36,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = "17"
     }
-
     kotlin {
         jvmToolchain(17)
     }
-
     buildFeatures {
         compose = true
     }
@@ -77,14 +56,12 @@ android {
         }
     }
     testOptions {
-        execution = "ANDROIDX_TEST_ORCHESTRATOR"
         packagingOptions {
             jniLibs {
                 useLegacyPackaging = true
             }
         }
         unitTests.isReturnDefaultValues = true
-        unitTests.isIncludeAndroidResources = true
     }
 }
 
@@ -101,12 +78,9 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.navigation:navigation-compose:2.6.0-rc01")
     implementation("io.coil-kt:coil-compose:1.4.0")
-
     implementation("com.google.accompanist:accompanist-permissions:0.31.0-alpha")
-
     implementation("com.google.maps.android:maps-compose:4.3.0")
     implementation("com.google.maps.android:maps-compose-utils:4.3.0")
-
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -122,8 +96,6 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx:24.10.3")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
     implementation("com.google.firebase:firebase-database:20.3.1")
-    implementation("androidx.test:monitor:1.6.1")
-    implementation("androidx.test.uiautomator:uiautomator:2.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -131,22 +103,16 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling:1.4.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.0")
-
     androidTestImplementation("com.kaspersky.android-components:kaspresso:1.4.3")
     // Allure support
     androidTestImplementation("com.kaspersky.android-components:kaspresso-allure-support:1.4.3")
     // Jetpack Compose support
     androidTestImplementation("com.kaspersky.android-components:kaspresso-compose-support:1.4.1")
-
     implementation("androidx.fragment:fragment:1.5.5")
-
     implementation("com.squareup.okhttp3:okhttp:3.10.0")
-
     testImplementation("org.mockito:mockito-core:3.11.2")
-    androidTestImplementation("org.mockito:mockito-core:3.11.2")
     testImplementation("org.mockito:mockito-inline:2.13.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-
     // Dependency for using Intents in instrumented tests
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
 
@@ -155,31 +121,26 @@ dependencies {
     androidTestImplementation("io.mockk:mockk:1.13.7")
     androidTestImplementation("io.mockk:mockk-android:1.13.7")
     androidTestImplementation("io.mockk:mockk-agent:1.13.7")
-
-    testImplementation("org.robolectric:robolectric:4.11.1")
 }
-
 tasks.register("jacocoTestReport", JacocoReport::class) {
     mustRunAfter("testDebugUnitTest", "connectedDebugAndroidTest")
-
     reports {
         xml.required = true
         html.required = true
     }
-
     val fileFilter = listOf(
         "**/R.class",
         "**/R$*.class",
         "**/BuildConfig.*",
         "**/Manifest*.*",
         "**/*Test*.*",
-        "android/**/*.*"
+        "android/**/*.*",
+        "**SignatureChecks.*"
     )
     val debugTree = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
         exclude(fileFilter)
     }
     val mainSrc = "${project.projectDir}/src/main/java"
-
     sourceDirectories.setFrom(files(mainSrc))
     classDirectories.setFrom(files(debugTree))
     executionData.setFrom(fileTree(project.buildDir) {
@@ -187,7 +148,6 @@ tasks.register("jacocoTestReport", JacocoReport::class) {
         include("outputs/code_coverage/debugAndroidTest/connected/*/coverage.ec")
     })
 }
-
 // Avoid redundant tests, debug is sufficient
 tasks.withType<Test> {
     onlyIf {
--- app/src/androidTest/java/com/github/se/studybuddies/screens/CreateGroupScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.github.se.studybuddies.screens
-
-import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
-import io.github.kakaocup.compose.node.element.ComposeScreen
-
-class CreateGroupScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
-    ComposeScreen<CreateGroupScreen>(
-        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("CreateGroup") }) {}
