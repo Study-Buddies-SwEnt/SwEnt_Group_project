81d6f3c5f47446b1ae2898f1da7e52804a0e8804
TdVBd <117034216+TdVBd@users.noreply.github.com>
2024-04-25 14:21:35 +0200

    ktfmt formating

--- app/src/main/java/com/github/se/studybuddies/database/DatabaseConnection.kt
+++ app/src/main/java/com/github/se/studybuddies/database/DatabaseConnection.kt
@@ -14,6 +14,7 @@ import com.github.se.studybuddies.data.todo.ToDoStatus
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
