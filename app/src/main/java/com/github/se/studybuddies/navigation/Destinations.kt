3096ca75f871fefe51316a936e5d5febda094f01
TdVBd <117034216+TdVBd@users.noreply.github.com>
2024-05-02 17:06:27 +0200

    changed the Destination file to solve a reference issue to Resources

--- app/src/main/java/com/github/se/studybuddies/navigation/Destinations.kt
+++ app/src/main/java/com/github/se/studybuddies/navigation/Destinations.kt
@@ -1,42 +1,26 @@
 package com.github.se.studybuddies.navigation
 
-import android.content.res.Resources
+import android.content.Context
 import com.github.se.studybuddies.R
 
-data class Destination(val route: String, val icon: Int = 0, val textId: String)
+data class Destination(val route: String, val icon: Int = 0, val textId: Int)
+
+fun getLocalizedText(context: Context, resourceId: Int): String {
+  return context.getString(resourceId)
+}
 
 val SETTINGS_DESTINATIONS =
     listOf(
-        Destination(
-            route = Route.SETTINGS,
-            icon = R.drawable.settings,
-            textId = Resources.getSystem().getString(R.string.settings)),
-        Destination(
-            route = Route.ACCOUNT,
-            icon = R.drawable.user,
-            textId = Resources.getSystem().getString(R.string.account)))
+        Destination(route = Route.SETTINGS, icon = R.drawable.settings, textId = R.string.settings),
+        Destination(route = Route.ACCOUNT, icon = R.drawable.user, textId = R.string.account))
 
 val GROUPS_SETTINGS_DESTINATIONS =
-    listOf(
-        Destination(
-            route = Route.GROUP, textId = Resources.getSystem().getString(R.string.modify_a_group)))
+    listOf(Destination(route = Route.GROUP, textId = R.string.modify_a_group))
 
 val BOTTOM_NAVIGATION_DESTINATIONS =
-    // 4rth route is a placeholder
     listOf(
         Destination(
-            route = Route.SOLOSTUDYHOME,
-            icon = R.drawable.user_v2,
-            textId = Resources.getSystem().getString(R.string.solo_study)),
-        Destination(
-            route = Route.GROUPSHOME,
-            icon = R.drawable.groups,
-            textId = Resources.getSystem().getString(R.string.groups)),
-        Destination(
-            route = Route.CHAT,
-            icon = R.drawable.messages,
-            textId = Resources.getSystem().getString(R.string.messages)),
-        Destination(
-            route = Route.TODOLIST,
-            icon = R.drawable.map,
-            textId = Resources.getSystem().getString(R.string.map)))
+            route = Route.SOLOSTUDYHOME, icon = R.drawable.user_v2, textId = R.string.solo_study),
+        Destination(route = Route.GROUPSHOME, icon = R.drawable.groups, textId = R.string.groups),
+        Destination(route = Route.CHAT, icon = R.drawable.messages, textId = R.string.messages),
+        Destination(route = Route.TODOLIST, icon = R.drawable.map, textId = R.string.map))
