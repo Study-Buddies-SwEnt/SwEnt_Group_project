de18867c0d64ad82236f8464769ada9e87aa8235
TdVBd <117034216+TdVBd@users.noreply.github.com>
2024-05-02 20:10:40 +0200

    ktfmt formating

--- app/src/main/java/com/github/se/studybuddies/MainActivity.kt
+++ app/src/main/java/com/github/se/studybuddies/MainActivity.kt
@@ -17,8 +17,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.github.se.studybuddies.mapService.LocationApp
 import com.github.se.studybuddies.data.Chat
+import com.github.se.studybuddies.mapService.LocationApp
 import com.github.se.studybuddies.navigation.NavigationActions
 import com.github.se.studybuddies.navigation.Route
 import com.github.se.studybuddies.ui.ChatScreen
