3c63f0ac789c817093e9f789e58b876ccb55778e
Ovoland <oceane.voland@epfl.ch>
2024-05-03 18:16:32 +0200

    Code reformating

--- app/src/main/java/com/github/se/studybuddies/ui/groups/GroupScreen.kt
+++ app/src/main/java/com/github/se/studybuddies/ui/groups/GroupScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.MoreVert
