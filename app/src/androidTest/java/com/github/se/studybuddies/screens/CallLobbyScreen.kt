6f39a11c362677a433db08f02b050dee992ae559
lorie <freez3moon@gmail.com>
2024-05-11 20:44:29 +0200

    New test files

--- app/src/androidTest/java/com/github/se/studybuddies/screens/CallLobbyScreen.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/screens/CallLobbyScreen.kt
@@ -1,4 +1,3 @@
 package com.github.se.studybuddies.screens
 
-class CallLobbyScreen {
-}
+class CallLobbyScreen {}
--- app/src/androidTest/java/com/github/se/studybuddies/tests/CallLobbyTest.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/tests/CallLobbyTest.kt
@@ -1,4 +1,3 @@
 package com.github.se.studybuddies.tests
 
-class CallLobbyTest {
-}
+class CallLobbyTest {}
