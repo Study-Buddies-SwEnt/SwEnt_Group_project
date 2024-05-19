da23b95b93ace0aaea2a1ae2299e743f508d9e75
Ovoland <oceane.voland@epfl.ch>
2024-05-19 23:50:00 +0200

    Remove End to end test

--- app/src/androidTest/java/com/github/se/studybuddies/endToEndTests/GroupCreateJoin.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/endToEndTests/GroupCreateJoin.kt
@@ -1,48 +1,15 @@
 package com.github.se.studybuddies.endToEndTests
 
-import android.net.Uri
-import androidx.compose.ui.test.assertTextContains
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.performTextClearance
-import androidx.compose.ui.test.performTextInput
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.test.espresso.Espresso
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.se.studybuddies.data.User
-import com.github.se.studybuddies.database.DbRepository
-import com.github.se.studybuddies.navigation.NavigationActions
-import com.github.se.studybuddies.navigation.Route
-import com.github.se.studybuddies.screens.CreateAccountScreen
-import com.github.se.studybuddies.screens.LoginScreen
-import com.github.se.studybuddies.ui.account.AccountSettings
-import com.github.se.studybuddies.ui.account.CreateAccount
-import com.github.se.studybuddies.ui.account.LoginScreen
-import com.github.se.studybuddies.ui.groups.CreateGroup
-import com.github.se.studybuddies.ui.groups.GroupsHome
-import com.github.se.studybuddies.ui.settings.Settings
-import com.github.se.studybuddies.ui.solo_study.SoloStudyHome
-import com.github.se.studybuddies.ui.theme.StudyBuddiesTheme
-import com.github.se.studybuddies.utility.fakeDatabase.MockDatabase
-import com.github.se.studybuddies.viewModels.GroupViewModel
-import com.github.se.studybuddies.viewModels.GroupsHomeViewModel
-import com.github.se.studybuddies.viewModels.UserViewModel
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
-import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.mockk.junit4.MockKRule
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class GroupCreateJoin : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
-  @get:Rule val composeTestRule = createComposeRule()
+  /*
+    @get:Rule val composeTestRule = createComposeRule()
   @get:Rule val mockkRule = MockKRule(this)
 
   lateinit var navigationActions: NavigationActions
@@ -166,5 +133,5 @@ class GroupCreateJoin : TestCase(kaspressoBuilder = Kaspresso.Builder.withCompos
           }
         }
            */
-  }
+  }*/
 }
