18ef14e4e516cf2fecb2ed420cadc4c6d6e2956d
Ovoland <oceane.voland@epfl.ch>
2024-05-21 21:48:19 +0200

    Started the E2E test verification

--- app/src/androidTest/java/com/github/se/studybuddies/endToEndTests/GroupCreateJoin.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/endToEndTests/GroupCreateJoin.kt
@@ -1,11 +1,46 @@
 package com.github.se.studybuddies.endToEndTests
 
-/*
+import android.net.Uri
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.test.espresso.Espresso
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.se.studybuddies.data.User
+import com.github.se.studybuddies.database.DbRepository
+import com.github.se.studybuddies.navigation.NavigationActions
+import com.github.se.studybuddies.navigation.Route
+import com.github.se.studybuddies.screens.CreateAccountScreen
+import com.github.se.studybuddies.ui.account.AccountSettings
+import com.github.se.studybuddies.ui.account.CreateAccount
+import com.github.se.studybuddies.ui.account.LoginScreen
+import com.github.se.studybuddies.ui.groups.CreateGroup
+import com.github.se.studybuddies.ui.groups.GroupsHome
+import com.github.se.studybuddies.ui.settings.Settings
+import com.github.se.studybuddies.ui.solo_study.SoloStudyHome
+import com.github.se.studybuddies.ui.theme.StudyBuddiesTheme
+import com.github.se.studybuddies.utility.fakeDatabase.MockDatabase
+import com.github.se.studybuddies.viewModels.GroupViewModel
+import com.github.se.studybuddies.viewModels.GroupsHomeViewModel
+import com.github.se.studybuddies.viewModels.UserViewModel
+import com.kaspersky.components.composesupport.config.withComposeSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.mockk.junit4.MockKRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
 @RunWith(AndroidJUnit4::class)
 class GroupCreateJoin : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
 
-    @get:Rule val composeTestRule = createComposeRule()
-  @get:Rule val mockkRule = MockKRule(this)
+  @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule private val mockkRule = MockKRule(this)
 
   lateinit var navigationActions: NavigationActions
 
@@ -53,9 +88,8 @@ class GroupCreateJoin : TestCase(kaspressoBuilder = Kaspresso.Builder.withCompos
           composable(Route.CREATEACCOUNT) { CreateAccount(userVM, navigationActions) }
           composable(Route.SOLOSTUDYHOME) { SoloStudyHome(navigationActions) }
           composable(
-              route = "${Route.SETTINGS}/{backRoute}",
-              arguments = listOf(navArgument("backRoute") { type = NavType.StringType })) {
-                  backStackEntry ->
+              "${Route.SETTINGS}/{backRoute}",
+              listOf(navArgument("backRoute") { type = NavType.StringType })) { backStackEntry ->
                 val backRoute = backStackEntry.arguments?.getString("backRoute")
                 if (backRoute != null) {
                   Settings(backRoute, navigationActions)
@@ -129,4 +163,4 @@ class GroupCreateJoin : TestCase(kaspressoBuilder = Kaspresso.Builder.withCompos
         }
            */
   }
-}*/
+}
