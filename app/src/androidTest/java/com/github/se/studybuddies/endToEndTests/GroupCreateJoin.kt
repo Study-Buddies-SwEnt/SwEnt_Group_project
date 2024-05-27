ef01943db7abc7f6699547773277b55313b89e29
Ovoland <oceane.voland@epfl.ch>
2024-05-27 17:06:08 +0200

    Try try

--- app/src/androidTest/java/com/github/se/studybuddies/endToEndTests/GroupCreateJoin.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/endToEndTests/GroupCreateJoin.kt
@@ -1,18 +1,20 @@
 package com.github.se.studybuddies.endToEndTests
 
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.se.studybuddies.database.MockDatabase
+import com.github.se.studybuddies.navigation.NavigationActions
 import com.github.se.studybuddies.screens.CreateAccountScreen
-import com.github.se.studybuddies.testUtilities.MockMainActivity
+import com.github.se.studybuddies.ui.account.CreateAccount
+import com.github.se.studybuddies.viewModels.UserViewModel
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
 import org.junit.Before
 import org.junit.Rule
@@ -24,24 +26,52 @@ class GroupCreateJoin : TestCase(kaspressoBuilder = Kaspresso.Builder.withCompos
   @get:Rule val composeTestRule = createComposeRule()
 
   @get:Rule val mockkRule = MockKRule(this)
+  @RelaxedMockK lateinit var mockNavActions: NavigationActions
 
+  /*
   @Before
   fun setUp() {
     ActivityScenario.launch(MockMainActivity::class.java)
+  }*/
+  val uid = "userTest"
+  private val db = MockDatabase()
+  var userVM = UserViewModel(uid, db)
+
+  @Before
+  fun testSetup() {
+    composeTestRule.setContent { CreateAccount(userVM, mockNavActions) }
   }
 
   @Test
-  fun userFlow1() {
+  fun elementsAreDisplayed() {
     ComposeScreen.onComposeScreen<CreateAccountScreen>(composeTestRule) {
+      saveButton { assertIsNotEnabled() }
       usernameField {
         performTextClearance()
         performTextInput("test user")
         assertTextContains("test user")
       }
+      Espresso.closeSoftKeyboard()
+      saveButton {
+        performScrollTo()
+        assertIsEnabled()
+        performClick()
+      }
     }
-    Espresso.closeSoftKeyboard()
-    composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag("save_button_account").performScrollTo().performClick()
+
+    /*
+    @Test
+    fun userFlow1() {
+      ComposeScreen.onComposeScreen<CreateAccountScreen>(composeTestRule) {
+        usernameField {
+          performTextClearance()
+          performTextInput("test user")
+          assertTextContains("test user")
+        }
+      }
+      Espresso.closeSoftKeyboard()
+      composeTestRule.waitForIdle()
+      composeTestRule.onNodeWithTag("save_button_account").performScrollTo().performClick()*/
     /*
     ComposeScreen.onComposeScreen<CreateAccountScreen>(composeTestRule) {
       // Create account
