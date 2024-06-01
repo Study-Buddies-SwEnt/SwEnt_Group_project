9a2b39eb615783dd2ccd626abd9cfd34ec537d71
TdVBd <117034216+TdVBd@users.noreply.github.com>
2024-06-01 23:05:52 +0200

    ktfmt formating

--- app/src/androidTest/java/com/github/se/studybuddies/tests/EditToDoTest.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/tests/EditToDoTest.kt
@@ -33,212 +33,212 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class EditToDoTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
 
-    @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    // This rule automatic initializes lateinit properties with @MockK, @RelaxedMockK, etc.
-    @get:Rule val mockkRule = MockKRule(this)
+  // This rule automatic initializes lateinit properties with @MockK, @RelaxedMockK, etc.
+  @get:Rule val mockkRule = MockKRule(this)
 
-    // Relaxed mocks methods have a default implementation returning values
-    @RelaxedMockK lateinit var mockNavActions: NavigationActions
-    private lateinit var toDoListViewModel: ToDoListViewModel
+  // Relaxed mocks methods have a default implementation returning values
+  @RelaxedMockK lateinit var mockNavActions: NavigationActions
+  private lateinit var toDoListViewModel: ToDoListViewModel
 
-    private val testID = "EditToDoTest"
-    private val testTask = ToDo(testID, "Name", LocalDate.now(), "Description", ToDoStatus.CREATED)
+  private val testID = "EditToDoTest"
+  private val testTask = ToDo(testID, "Name", LocalDate.now(), "Description", ToDoStatus.CREATED)
 
-    @Before
-    fun testSetup() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        toDoListViewModel = ToDoListViewModel(context)
-        toDoListViewModel.addToDo(testTask)
-        composeTestRule.setContent { EditToDoScreen(testID, toDoListViewModel, mockNavActions) }
+  @Before
+  fun testSetup() {
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    toDoListViewModel = ToDoListViewModel(context)
+    toDoListViewModel.addToDo(testTask)
+    composeTestRule.setContent { EditToDoScreen(testID, toDoListViewModel, mockNavActions) }
+  }
+
+  @Test
+  fun topAppBarTest() = run {
+    onComposeScreen<EditToDoScreen>(composeTestRule) {
+      topAppBox {
+        // arrange: verify pre-conditions
+        assertIsDisplayed()
+      }
+
+      topAppBar {
+        // arrange: verify pre-conditions
+        assertIsDisplayed()
+      }
+      divider {
+        // arrange: verify pre-conditions
+        assertIsDisplayed()
+      }
+      goBackButton {
+        // arrange: verify pre-conditions
+        assertIsDisplayed()
+        performClick()
+      }
     }
+    // assert: the nav action has been called
+    verify { mockNavActions.navigateTo(Route.TODOLIST) }
+    confirmVerified(mockNavActions)
+  }
 
-    @Test
-    fun topAppBarTest() = run {
-        onComposeScreen<EditToDoScreen>(composeTestRule) {
-            topAppBox {
-                // arrange: verify pre-conditions
-                assertIsDisplayed()
-            }
-
-            topAppBar {
-                // arrange: verify pre-conditions
-                assertIsDisplayed()
-            }
-            divider {
-                // arrange: verify pre-conditions
-                assertIsDisplayed()
-            }
-            goBackButton {
-                // arrange: verify pre-conditions
-                assertIsDisplayed()
-                performClick()
-            }
-        }
-        // assert: the nav action has been called
-        verify { mockNavActions.navigateTo(Route.TODOLIST) }
-        confirmVerified(mockNavActions)
+  @Test
+  fun inputTaskName() {
+    onComposeScreen<EditToDoScreen>(composeTestRule) {
+      todoNameField {
+        performTextClearance()
+        performTextInput("Name")
+        assertTextContains("Name")
+      }
+      ViewActions.closeSoftKeyboard()
+      saveButton {
+        assertIsEnabled()
+        performClick()
+      }
+      verify { mockNavActions.goBack() }
+      confirmVerified(mockNavActions)
     }
+  }
 
-    @Test
-    fun inputTaskName() {
-        onComposeScreen<EditToDoScreen>(composeTestRule) {
-            todoNameField {
-                performTextClearance()
-                performTextInput("Name")
-                assertTextContains("Name")
-            }
-            ViewActions.closeSoftKeyboard()
-            saveButton {
-                assertIsEnabled()
-                performClick()
-            }
-            verify { mockNavActions.goBack() }
-            confirmVerified(mockNavActions)
-        }
+  @Test
+  fun inputDescription() {
+    onComposeScreen<EditToDoScreen>(composeTestRule) {
+      todoDescriptionField {
+        performTextClearance()
+        performTextInput("Description")
+        assertTextContains("Description")
+      }
+      ViewActions.closeSoftKeyboard()
+      saveButton {
+        assertIsEnabled()
+        performClick()
+      }
+      verify { mockNavActions.goBack() }
+      confirmVerified(mockNavActions)
     }
+  }
 
-    @Test
-    fun inputDescription() {
-        onComposeScreen<EditToDoScreen>(composeTestRule) {
-            todoDescriptionField {
-                performTextClearance()
-                performTextInput("Description")
-                assertTextContains("Description")
-            }
-            ViewActions.closeSoftKeyboard()
-            saveButton {
-                assertIsEnabled()
-                performClick()
-            }
-            verify { mockNavActions.goBack() }
-            confirmVerified(mockNavActions)
-        }
+  @Test
+  fun datePickerTest() {
+    onComposeScreen<EditToDoScreen>(composeTestRule) {
+      todoDateField { performClick() }
+      datePicker { assertIsDisplayed() }
+      dateConfirmButton {
+        assertIsDisplayed()
+        assertHasClickAction()
+        assertTextEquals(("Confirm"))
+      }
+      dateDismissButton {
+        assertIsDisplayed()
+        assertHasClickAction()
+        assertTextEquals(("Cancel"))
+        performClick()
+      }
+      datePicker { assertIsNotDisplayed() }
     }
+  }
 
-    @Test
-    fun datePickerTest() {
-        onComposeScreen<EditToDoScreen>(composeTestRule) {
-            todoDateField { performClick() }
-            datePicker { assertIsDisplayed() }
-            dateConfirmButton {
-                assertIsDisplayed()
-                assertHasClickAction()
-                assertTextEquals(("Confirm"))
-            }
-            dateDismissButton {
-                assertIsDisplayed()
-                assertHasClickAction()
-                assertTextEquals(("Cancel"))
-                performClick()
-            }
-            datePicker { assertIsNotDisplayed() }
-        }
+  /*
+  @Test
+  fun inputDate() {
+    onComposeScreen<EditToDoScreen>(
+      composeTestRule
+    ) {
+      todoDateField{
+        performClick()
+      }
+
+      datePicker {
+        PickerActions.setDate(2024,5,21)
+      }
+      //onView(withClassName(equalTo(DatePicker::class.java.name))).perform(PickerActions.setDate(2024, 5, 21))
+
+      dateConfirmButton{
+        performClick()
+      }
+      datePicker {
+        assertIsNotDisplayed()
+      }
+
+      //composeTestRule.onNodeWithTag("todoDateField").assertTextContains("Tue May 21 2024")
+      todoDateField{
+        assertTextContains("")
+      }
+
+
+      todoNameField {
+        performTextClearance()
+        performTextInput("Official Task Testing")
+      }
+      ViewActions.closeSoftKeyboard()
+      saveButton {
+        assertIsEnabled()
+        performClick()
+      }
+      verify { mockNavActions.goBack() }
+      confirmVerified(mockNavActions)
     }
+  }
 
-    /*
-    @Test
-    fun inputDate() {
-      onComposeScreen<EditToDoScreen>(
-        composeTestRule
-      ) {
-        todoDateField{
-          performClick()
-        }
+   */
 
-        datePicker {
-          PickerActions.setDate(2024,5,21)
-        }
-        //onView(withClassName(equalTo(DatePicker::class.java.name))).perform(PickerActions.setDate(2024, 5, 21))
-
-        dateConfirmButton{
-          performClick()
-        }
-        datePicker {
-          assertIsNotDisplayed()
-        }
-
-        //composeTestRule.onNodeWithTag("todoDateField").assertTextContains("Tue May 21 2024")
-        todoDateField{
-          assertTextContains("")
-        }
-
-
+  @Test
+  fun saveTaskDoesNotWorkWithEmptyTitle() = run {
+    onComposeScreen<EditToDoScreen>(composeTestRule) {
+      step("Open EditToDo screen") {
         todoNameField {
+          assertIsDisplayed()
+          // interact with the text field
+          performClick()
+          // clear the text field
           performTextClearance()
-          performTextInput("Official Task Testing")
         }
         ViewActions.closeSoftKeyboard()
+
         saveButton {
-          assertIsEnabled()
+          // arrange: verify pre-conditions
+          assertIsDisplayed()
+          assertIsNotEnabled()
+
+          // act: click on the save button
           performClick()
         }
-        verify { mockNavActions.goBack() }
+        // verify that the nav action has not been called
+        verify { mockNavActions wasNot Called }
         confirmVerified(mockNavActions)
       }
     }
+  }
 
-     */
-
-    @Test
-    fun saveTaskDoesNotWorkWithEmptyTitle() = run {
-        onComposeScreen<EditToDoScreen>(composeTestRule) {
-            step("Open EditToDo screen") {
-                todoNameField {
-                    assertIsDisplayed()
-                    // interact with the text field
-                    performClick()
-                    // clear the text field
-                    performTextClearance()
-                }
-                ViewActions.closeSoftKeyboard()
-
-                saveButton {
-                    // arrange: verify pre-conditions
-                    assertIsDisplayed()
-                    assertIsNotEnabled()
-
-                    // act: click on the save button
-                    performClick()
-                }
-                // verify that the nav action has not been called
-                verify { mockNavActions wasNot Called }
-                confirmVerified(mockNavActions)
-            }
-        }
+  @Test
+  fun elementsAreDisplayed() {
+    onComposeScreen<EditToDoScreen>(composeTestRule) {
+      runBlocking {
+        delay(6000) // Adjust the delay time as needed
+      }
+      editTodoCol { assertIsDisplayed() }
+      todoNameField {
+        assertIsDisplayed()
+        assertTextContains("Name")
+        assertHasClickAction()
+      }
+      todoDescriptionField {
+        assertIsDisplayed()
+        assertTextContains("Description")
+        assertHasClickAction()
+      }
+      todoDateField {
+        assertIsDisplayed()
+        assertHasClickAction()
+      }
+      saveButton {
+        assertIsDisplayed()
+        assertHasClickAction()
+        assertTextEquals(("Save"))
+      }
+      deleteButton {
+        assertIsDisplayed()
+        assertHasClickAction()
+        assertTextEquals(("Delete"))
+      }
     }
-
-    @Test
-    fun elementsAreDisplayed() {
-        onComposeScreen<EditToDoScreen>(composeTestRule) {
-            runBlocking {
-                delay(6000) // Adjust the delay time as needed
-            }
-            editTodoCol { assertIsDisplayed() }
-            todoNameField {
-                assertIsDisplayed()
-                assertTextContains("Name")
-                assertHasClickAction()
-            }
-            todoDescriptionField {
-                assertIsDisplayed()
-                assertTextContains("Description")
-                assertHasClickAction()
-            }
-            todoDateField {
-                assertIsDisplayed()
-                assertHasClickAction()
-            }
-            saveButton {
-                assertIsDisplayed()
-                assertHasClickAction()
-                assertTextEquals(("Save"))
-            }
-            deleteButton {
-                assertIsDisplayed()
-                assertHasClickAction()
-                assertTextEquals(("Delete"))
-            }
-        }
-    }
+  }
 }
