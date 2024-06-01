caa7f4c3de77778d73a73cbe16d3d61fd25a8828
Oliver-Tuf <oollii@live.fr>
2024-06-01 23:49:14 +0200

    Correct small bug

--- app/src/main/java/com/github/se/studybuddies/ui/groups/CreateGroup.kt
+++ app/src/main/java/com/github/se/studybuddies/ui/groups/CreateGroup.kt
@@ -24,9 +24,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.github.se.studybuddies.R
 import com.github.se.studybuddies.navigation.NavigationActions
+import com.github.se.studybuddies.navigation.Route
 import com.github.se.studybuddies.permissions.checkPermission
 import com.github.se.studybuddies.permissions.imagePermissionVersion
-import com.github.se.studybuddies.ui.shared_elements.GoBackRouteToLastPageButton
+import com.github.se.studybuddies.ui.shared_elements.GoBackRouteButton
 import com.github.se.studybuddies.ui.shared_elements.SaveButton
 import com.github.se.studybuddies.ui.shared_elements.SetProfilePicture
 import com.github.se.studybuddies.ui.shared_elements.Sub_title
@@ -72,7 +73,9 @@ fun CreateGroup(groupViewModel: GroupViewModel, navigationActions: NavigationAct
       topBar = {
         TopNavigationBar(
             title = { Sub_title(stringResource(R.string.create_group)) },
-            navigationIcon = { GoBackRouteToLastPageButton(navigationActions = navigationActions) },
+            navigationIcon = {
+              GoBackRouteButton(navigationActions = navigationActions, Route.GROUPSHOME)
+            },
             actions = {})
       }) {
         Column(
