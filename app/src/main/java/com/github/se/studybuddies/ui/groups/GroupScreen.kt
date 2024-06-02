d4c693592c96126bfedee93a07de3c096a6a5d91
hanelmouta <127690334+hanelmouta@users.noreply.github.com>
2024-06-02 18:02:54 +0200

    changes 5

--- app/src/main/java/com/github/se/studybuddies/ui/groups/GroupScreen.kt
+++ app/src/main/java/com/github/se/studybuddies/ui/groups/GroupScreen.kt
@@ -127,17 +127,13 @@ fun GroupScreen(
                     Destination(
                         route = "${Route.SHAREDTIMER}/$groupUID",
                         icon = R.drawable.timer,
-
                         textId = stringResource(R.string.timer)),
                     Destination(
                         //
                         route = "${Route.GROUPCALENDAR}/$groupUID",
                         icon = R.drawable.calendar,
                         textId = "Calendar")),
-
-                        textId = stringResource(R.string.timer))),
             currentRoute = Route.GROUP,
-
             iconSize = 32)
       }) { innerPadding ->
         Column(
