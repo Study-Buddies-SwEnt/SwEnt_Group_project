df31d50b57a57cdd85cfb4e4d753598a4dfbcba9
lorie <freez3moon@gmail.com>
2024-06-03 00:23:50 +0200

    solve test problem

--- app/src/androidTest/java/com/github/se/studybuddies/screens/SoloStudyScreen.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/screens/SoloStudyScreen.kt
@@ -7,7 +7,7 @@ import io.github.kakaocup.compose.node.element.KNode
 class SoloStudyScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<SoloStudyScreen>(
         semanticsProvider = semanticsProvider,
-        viewBuilderAction = { hasTestTag("Solo Study_menu") }) {
+        viewBuilderAction = { hasTestTag("Solo study_menu") }) {
 
   val drawerScaffold: KNode = onNode { hasTestTag("Solo study_drawer_scaffold") }
   val soloStudyScreen: KNode = drawerScaffold.child { hasTestTag("solo_study_home") }
@@ -18,10 +18,10 @@ class SoloStudyScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
   val timerButton: KNode = row2.child { hasTestTag("Timer_button") }
   val calendarButton: KNode = row2.child { hasTestTag("Calendar_button") }
 
-  val topAppBox: KNode = drawerScaffold.child { hasTestTag("Solo Study_top_app_box") }
-  val topAppBar: KNode = topAppBox.child { hasTestTag("Solo Study_top_app_bar") }
+  val topAppBox: KNode = drawerScaffold.child { hasTestTag("Solo study_top_app_box") }
+  val topAppBar: KNode = topAppBox.child { hasTestTag("Solo study_top_app_bar") }
   val drawerMenuButton: KNode = topAppBar.child { hasTestTag("drawer_menu_icon") }
-  val drawerSheet: KNode = onNode { hasTestTag("Solo Study_drawer_sheet") }
+  val drawerSheet: KNode = onNode { hasTestTag("Solo study_drawer_sheet") }
   val settingsButton: KNode = drawerSheet.child { hasTestTag("Settings_button") }
   val accountButton: KNode = drawerSheet.child { hasTestTag("Account_button") }
   val soloTitle: KNode = topAppBar.child { hasTestTag("main_title") }
