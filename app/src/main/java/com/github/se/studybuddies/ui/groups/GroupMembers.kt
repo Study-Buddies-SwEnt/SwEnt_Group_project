b5021b4e7161bfaaaa58e3802871bd8a30120eb7
TdVBd <117034216+TdVBd@users.noreply.github.com>
2024-06-02 22:07:17 +0200

    conflict resolution

--- app/src/main/java/com/github/se/studybuddies/ui/groups/GroupMembers.kt
+++ app/src/main/java/com/github/se/studybuddies/ui/groups/GroupMembers.kt
@@ -75,22 +75,22 @@ fun GroupMembers(
     db: DbRepository
 ) {
 
-  if (groupUID.isEmpty()) return
-  groupViewModel.fetchGroupData(groupUID)
-  val groupData by groupViewModel.group.observeAsState()
+    if (groupUID.isEmpty()) return
+    groupViewModel.fetchGroupData(groupUID)
+    val groupData by groupViewModel.group.observeAsState()
 
-  val nameState = remember { mutableStateOf(groupData?.name ?: "") }
-  val members = remember { groupData?.let { mutableStateOf(it.members) } }
+    val nameState = remember { mutableStateOf(groupData?.name ?: "") }
+    val members = remember { groupData?.let { mutableStateOf(it.members) } }
 
-  val isBoxVisible = remember { mutableStateOf(false) }
-  groupData?.let { nameState.value = it.name }
+    val isBoxVisible = remember { mutableStateOf(false) }
+    groupData?.let { nameState.value = it.name }
 
-  groupViewModel.fetchUsers()
-  val userData by groupViewModel.members.observeAsState()
+    groupViewModel.fetchUsers()
+    val userData by groupViewModel.members.observeAsState()
 
-  val currUser = groupViewModel.getCurrentUser()
+    val currUser = groupViewModel.getCurrentUser()
 
-  /*
+    /*
   var nbMember = 0
   groupData?.let {
     nameState.value = it.name
@@ -115,195 +115,214 @@ fun GroupMembers(
   userDatas.value = userDatas.value.toSet().toMutableList()
      */
 
-  Scaffold(
-      modifier = Modifier.fillMaxSize().background(White).testTag("members_scaffold"),
-      topBar = {
-        TopNavigationBar(
-            title = { Sub_title(stringResource(R.string.members)) },
-            leftButton = {
-              GoBackRouteButton(navigationActions = navigationActions, Route.GROUPSHOME)
-            },
-            rightButton = { GroupsSettingsButton(groupUID, navigationActions, db) })
-      }) { paddingValues ->
+    Scaffold(
+        modifier = Modifier.fillMaxSize().background(White).testTag("members_scaffold"),
+        topBar = {
+            TopNavigationBar(
+                title = { Sub_title(stringResource(R.string.members)) },
+                leftButton = {
+                    GoBackRouteButton(navigationActions = navigationActions, Route.GROUPSHOME)
+                },
+                rightButton = { GroupsSettingsButton(groupUID, navigationActions, db) })
+        }) { paddingValues ->
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Top,
         ) {}
         if (isBoxVisible.value) {
-          ShowContact(groupUID, groupViewModel, isBoxVisible)
+            ShowContact(groupUID, groupViewModel, isBoxVisible)
         } else {
-          LazyColumn(
-              modifier =
-                  Modifier.fillMaxSize().padding(paddingValues).testTag("draw_member_column"),
-              verticalArrangement = Arrangement.spacedBy(5.dp, Alignment.Top),
-              horizontalAlignment = Alignment.CenterHorizontally) {
+            LazyColumn(
+                modifier =
+                Modifier.fillMaxSize().padding(paddingValues).testTag("draw_member_column"),
+                verticalArrangement = Arrangement.spacedBy(5.dp, Alignment.Top),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 item { Name(nameState) }
                 item { Spacer(modifier = Modifier.padding(5.dp)) }
                 item { AddMemberButtonUID(groupUID, groupViewModel) }
                 item { AddMemberButtonList(isBoxVisible) }
                 item { Spacer(modifier = Modifier.padding(5.dp)) }
                 if (userData?.size!! > 0) {
-                  items(userData!!.size) { index ->
-                    val user = userData!![index]
-                    MemberItem(groupUID, navigationActions, db, user, currUser)
-                  }
+                    items(userData!!.size) { index ->
+                        val user = userData!![index]
+                        MemberItem(groupUID, navigationActions, db, user, currUser)
+                    }
                 } else { // Should never happen
-                  item {
-                    Text(
-                        stringResource(R.string.error_no_member_found_for_this_group),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.testTag("EmptyGroupMemberText"))
-                  }
+                    item {
+                        Text(
+                            stringResource(R.string.error_no_member_found_for_this_group),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.testTag("EmptyGroupMemberText")
+                        )
+                    }
                 }
-              }
+            }
         }
-      }
+    }
 
-  @Composable
-  fun Name(nameState: MutableState<String>) {
-    Spacer(Modifier.height(20.dp))
-    Text(
-        nameState.value,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.testTag("ShowGroupNameInGroupMember"))
-  }
+    @Composable
+    fun Name(nameState: MutableState<String>) {
+        Spacer(Modifier.height(20.dp))
+        Text(
+            nameState.value,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.testTag("ShowGroupNameInGroupMember")
+        )
+    }
 
-  @Composable
-  fun MemberItem(
-      groupUID: String,
-      user: User,
-      navigationActions: NavigationActions,
-      db: DbRepository,
-      userData: User,
-      currUser: String
-  ) {
-    Box(
-        modifier =
+    @Composable
+    fun MemberItem(
+        groupUID: String,
+        user: User,
+        navigationActions: NavigationActions,
+        db: DbRepository,
+        userData: User,
+        currUser: String
+    ) {
+        Box(
+            modifier =
             Modifier.fillMaxWidth()
                 .background(Color.White)
                 .drawBehind {
-                  val strokeWidth = 1f
-                  val y = size.height - strokeWidth / 2
-                  drawLine(Color.LightGray, Offset(0f, y), Offset(size.width, y), strokeWidth)
+                    val strokeWidth = 1f
+                    val y = size.height - strokeWidth / 2
+                    drawLine(Color.LightGray, Offset(0f, y), Offset(size.width, y), strokeWidth)
                 }
                 .testTag(userData.username + "_box")) {
-          Row(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
-            Box(modifier = Modifier.size(52.dp).clip(CircleShape).background(Color.Transparent)) {
-              Image(
-                  painter = rememberAsyncImagePainter(userData.photoUrl),
-                  contentDescription = stringResource(id = R.string.user_picture),
-                  modifier = Modifier.fillMaxSize(),
-                  contentScale = ContentScale.Crop)
+            Row(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+                Box(
+                    modifier = Modifier.size(52.dp).clip(CircleShape).background(Color.Transparent)
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(userData.photoUrl),
+                        contentDescription = stringResource(id = R.string.user_picture),
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(
+                    text = userData.username,
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    style = TextStyle(fontSize = 20.sp),
+                    lineHeight = 28.sp
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                MemberOptionButton(
+                    groupUID, userData.uid, userData.username, navigationActions, db, currUser
+                )
             }
-            Spacer(modifier = Modifier.size(16.dp))
-            Text(
-                text = userData.username,
-                modifier = Modifier.align(Alignment.CenterVertically),
-                style = TextStyle(fontSize = 20.sp),
-                lineHeight = 28.sp)
-            Spacer(modifier = Modifier.weight(1f))
-            MemberOptionButton(
-                groupUID, userData.uid, userData.username, navigationActions, db, currUser)
-          }
         }
-  }
-
-  @Composable
-  fun MemberOptionButton(
-      groupUID: String,
-      userUID: String,
-      username: String,
-      navigationActions: NavigationActions,
-      db: DbRepository,
-      currUser: String
-  ) {
-    var isRemoveUserDialogVisible by remember { mutableStateOf(false) }
-    val expandedState = remember { mutableStateOf(false) }
-    val groupViewModel = GroupViewModel(groupUID, db)
-    Row {
-      IconButton(
-          onClick = { expandedState.value = true },
-      ) {
-        Icon(
-            imageVector = Icons.Default.MoreVert,
-            tint = Blue,
-            contentDescription = stringResource(R.string.dots_menu))
-      }
-      DropdownMenu(
-          expanded = expandedState.value, onDismissRequest = { expandedState.value = false }) {
-            GROUPS_MEMBERS_DESTINATIONS.forEach { item ->
-              DropdownMenuItem(
-                  onClick = {
-                    expandedState.value = false
-                    when (item.route) {
-                      Route.USERREMOVE -> {
-                        isRemoveUserDialogVisible = true
-                      }
-                    }
-                  },
-                  text = {
-                    Spacer(modifier = Modifier.size(16.dp))
-                    Text(item.textId)
-                  })
-            }
-          }
     }
-    if (isRemoveUserDialogVisible) {
-      Dialog(onDismissRequest = { isRemoveUserDialogVisible = false }) {
-        Box(
-            modifier =
-                Modifier.width(280.dp)
-                    .height(140.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.White)) {
-              Column(
-                  modifier = Modifier.padding(16.dp),
-                  verticalArrangement = Arrangement.Center,
-                  horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(
-                        text = "Are you sure you want to remove $username from the group ?",
-                        color = Blue)
-                    Spacer(modifier = Modifier.height(20.dp))
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly) {
-                          Button(
-                              onClick = {
-                                groupViewModel.leaveGroup(groupUID, userUID)
-                                if (userUID == currUser) {
-                                  navigationActions.navigateTo(Route.GROUPSHOME)
-                                } else {
-                                  navigationActions.navigateTo("${Route.GROUPMEMBERS}/$groupUID")
+
+    @Composable
+    fun MemberOptionButton(
+        groupUID: String,
+        userUID: String,
+        username: String,
+        navigationActions: NavigationActions,
+        db: DbRepository,
+        currUser: String
+    ) {
+        var isRemoveUserDialogVisible by remember { mutableStateOf(false) }
+        val expandedState = remember { mutableStateOf(false) }
+        val groupViewModel = GroupViewModel(groupUID, db)
+        Row {
+            IconButton(
+                onClick = { expandedState.value = true },
+            ) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    tint = Blue,
+                    contentDescription = stringResource(R.string.dots_menu)
+                )
+            }
+            DropdownMenu(
+                expanded = expandedState.value,
+                onDismissRequest = { expandedState.value = false }) {
+                GROUPS_MEMBERS_DESTINATIONS.forEach { item ->
+                    DropdownMenuItem(
+                        onClick = {
+                            expandedState.value = false
+                            when (item.route) {
+                                Route.USERREMOVE -> {
+                                    isRemoveUserDialogVisible = true
                                 }
-                                isRemoveUserDialogVisible = false
-                              },
-                              modifier =
-                                  Modifier.clip(RoundedCornerShape(4.dp))
-                                      .width(80.dp)
-                                      .height(40.dp),
-                              colors =
-                                  ButtonDefaults.buttonColors(
-                                      containerColor = Color.Red, contentColor = White)) {
-                                Text(text = stringResource(R.string.yes))
-                              }
-
-                          Button(
-                              onClick = { isRemoveUserDialogVisible = false },
-                              modifier =
-                                  Modifier.clip(RoundedCornerShape(4.dp))
-                                      .width(80.dp)
-                                      .height(40.dp),
-                              colors =
-                                  ButtonDefaults.buttonColors(
-                                      containerColor = Blue, contentColor = White)) {
-                                Text(text = stringResource(R.string.no))
-                              }
-                        }
-                  }
+                            }
+                        },
+                        text = {
+                            Spacer(modifier = Modifier.size(16.dp))
+                            Text(item.textId)
+                        })
+                }
             }
-      }
+        }
+        if (isRemoveUserDialogVisible) {
+            Dialog(onDismissRequest = { isRemoveUserDialogVisible = false }) {
+                Box(
+                    modifier =
+                    Modifier.width(280.dp)
+                        .height(140.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color.White)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Are you sure you want to remove $username from the group ?",
+                            color = Blue
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            Button(
+                                onClick = {
+                                    groupViewModel.leaveGroup(groupUID, userUID)
+                                    if (userUID == currUser) {
+                                        navigationActions.navigateTo(Route.GROUPSHOME)
+                                    } else {
+                                        navigationActions.navigateTo("${Route.GROUPMEMBERS}/$groupUID")
+                                    }
+                                    isRemoveUserDialogVisible = false
+                                },
+                                modifier =
+                                Modifier.clip(RoundedCornerShape(4.dp))
+                                    .width(80.dp)
+                                    .height(40.dp),
+                                colors =
+                                ButtonDefaults.buttonColors(
+                                    containerColor = Color.Red, contentColor = White
+                                )
+                            ) {
+                                Text(text = stringResource(R.string.yes))
+                            }
+
+                            Button(
+                                onClick = { isRemoveUserDialogVisible = false },
+                                modifier =
+                                Modifier.clip(RoundedCornerShape(4.dp))
+                                    .width(80.dp)
+                                    .height(40.dp),
+                                colors =
+                                ButtonDefaults.buttonColors(
+                                    containerColor = Blue, contentColor = White
+                                )
+                            ) {
+                                Text(text = stringResource(R.string.no))
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-  }
+}
 
   @Composable
   fun AddMemberButtonList(isBoxVisible: MutableState<Boolean>) {
