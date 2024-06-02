6580da33dcd5ecaa19685fdb53c8668658e28578
Svannia <alice.reymond@epfl.ch>
2024-06-02 22:16:56 +0200

    complexity

--- app/src/main/java/com/github/se/studybuddies/ui/topics/TopicResources.kt
+++ app/src/main/java/com/github/se/studybuddies/ui/topics/TopicResources.kt
@@ -171,33 +171,33 @@ fun TopicResources(
                     horizontalAlignment = Alignment.Start,
                     content = {
                       if (areaState.value == FileArea.RESOURCES) {
-                         item {
-                             ShowResources(images, expandedImage, expandImage)
-                         }
+                        item { ShowResources(images, expandedImage, expandImage) }
                       } else {
                         items(strongUsers.value) { user -> UserBox(user) }
                       }
                     })
               }
         }
-      FullImage(expandImage, expandedImage.value) { expandImage.value = false }
+    FullImage(expandImage, expandedImage.value) { expandImage.value = false }
   }
 }
 
 @Composable
-fun ShowResources(images: MutableState<List<Uri>>, expandedImage: MutableState<Uri>, expandImage: MutableState<Boolean>) {
-    if (images.value.isEmpty()) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            Text(stringResource(R.string.no_resources_yet))
-        }
-    } else {
-       images.value.forEach { image ->
-            ResourceImage(image) {
-                expandedImage.value = image
-                expandImage.value = true
-            }
-        }
+fun ShowResources(
+    images: MutableState<List<Uri>>,
+    expandedImage: MutableState<Uri>,
+    expandImage: MutableState<Boolean>
+) {
+  if (images.value.isEmpty()) {
+    Column(modifier = Modifier.fillMaxSize()) { Text(stringResource(R.string.no_resources_yet)) }
+  } else {
+    images.value.forEach { image ->
+      ResourceImage(image) {
+        expandedImage.value = image
+        expandImage.value = true
+      }
     }
+  }
 }
 
 @Composable
@@ -304,44 +304,44 @@ fun FullImage(show: MutableState<Boolean>, image: Uri, onDismiss: () -> Unit) {
   val offsetX = remember { mutableStateOf(0f) }
   val offsetY = remember { mutableStateOf(0f) }
 
-    if (show.value) {
-        Column(
-            modifier = Modifier.fillMaxSize().background(color = Color.Black.copy(alpha = 0.8f)),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top) {
-            Row(
-                modifier = Modifier.fillMaxWidth().background(color = Color.Black),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically) {
+  if (show.value) {
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = Color.Black.copy(alpha = 0.8f)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top) {
+          Row(
+              modifier = Modifier.fillMaxWidth().background(color = Color.Black),
+              horizontalArrangement = Arrangement.End,
+              verticalAlignment = Alignment.CenterVertically) {
                 IconButton(onClick = { onDismiss() }) {
-                    Icon(
-                        painter = painterResource(R.drawable.dismiss),
-                        contentDescription = stringResource(R.string.dismiss_button))
+                  Icon(
+                      painter = painterResource(R.drawable.dismiss),
+                      contentDescription = stringResource(R.string.dismiss_button))
                 }
-            }
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier =
-                Modifier.fillMaxSize().pointerInput(Unit) {
+              }
+          Box(
+              contentAlignment = Alignment.Center,
+              modifier =
+                  Modifier.fillMaxSize().pointerInput(Unit) {
                     detectTransformGestures { _, pan, zoom, _ ->
-                        scale.value *= zoom
-                        offsetX.value += pan.x
-                        offsetY.value += pan.y
+                      scale.value *= zoom
+                      offsetX.value += pan.x
+                      offsetY.value += pan.y
                     }
-                }) {
+                  }) {
                 Image(
                     painter = rememberAsyncImagePainter(image.toString()),
                     contentDescription = stringResource(R.string.image_resource),
                     contentScale = ContentScale.None,
                     modifier =
-                    Modifier.graphicsLayer {
-                        scaleX = scale.value
-                        scaleY = scale.value
-                        translationX = offsetX.value
-                        translationY = offsetY.value
-                    }
-                        .fillMaxSize())
-            }
+                        Modifier.graphicsLayer {
+                              scaleX = scale.value
+                              scaleY = scale.value
+                              translationX = offsetX.value
+                              translationY = offsetY.value
+                            }
+                            .fillMaxSize())
+              }
         }
-    }
+  }
 }
