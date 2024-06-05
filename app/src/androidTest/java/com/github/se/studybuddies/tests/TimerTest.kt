9d9952b0d685cb4ae34e3b0234f048888a524362
TdVBd <117034216+TdVBd@users.noreply.github.com>
2024-06-05 20:11:35 +0200

    corrected some tets

--- app/src/androidTest/java/com/github/se/studybuddies/tests/TimerTest.kt
+++ app/src/androidTest/java/com/github/se/studybuddies/tests/TimerTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.studybuddies.navigation.NavigationActions
-import com.github.se.studybuddies.navigation.Route
 import com.github.se.studybuddies.screens.TimerScreen
 import com.github.se.studybuddies.ui.timer.TimerScreenContent
 import com.github.se.studybuddies.viewModels.TimerViewModel
